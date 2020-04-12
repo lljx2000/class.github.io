f7b4555c8ca58fa6b838d7128151a05eff6db413
ljx <1026796388@qq.com>
2020-04-12 17:10:20 +0800

    2

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 var ShowInput = true;
 function showInputTable() {
     if (showInput) {
-        ele("AddInput").style.display = "none";
+        document.getElementById("AddInput").style.display = "none";
         showInput = false;
     }
     else {
-        ele("AddInput").style.display = "block";
+        document.getElementById("AddInput").style.display = "block";
         showInput = true;
     }
 }
 // 更新课程
 function updateInfo() {
-    ele("title_h").innerText = ele("title_in").value;
+    var v = document.getElementById("title_in").value;
+    document.getElementById("title_h").innerHTML = v;
 }
